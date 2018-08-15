4ed8c36d1732cf4f899962f0f81630a24f8c40a7
Nicole Richardson <njrichardson95@gmail.com>
2018-08-15 16:34:45 -0400

    added in mysql data and ran it to create database, also decided on a theme

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Poblano Picasso Burger', 0);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Eggers Cant Be Cheesers', 0);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeses Is Born Burger', 0);
