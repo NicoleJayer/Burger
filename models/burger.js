1acc7ec646538c5f4784a7f281018d556ba9d398
Nicole Richardson <njrichardson95@gmail.com>
2018-08-18 15:22:00 -0400

    got create burger to work

--- models/burger.js
+++ models/burger.js
@@ -9,7 +9,13 @@ var burger = {
 
   update: function(id,cb){
     orm.update('burgers',id,cb);
+  },
+
+  create: function(name,cb){
+    orm.create('burgers', name, cb);
   }
+
+
 }
 
 module.exports = burger;
