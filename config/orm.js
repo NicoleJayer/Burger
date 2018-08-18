dee5453e75fe130d9aa26fc831fbce09b2f5a1d1
Nicole Richardson <njrichardson95@gmail.com>
2018-08-18 00:27:20 -0400

    server.js boiler plate setup

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,28 @@
 // Import the MySQL connection object
 var connection = require ('./connection.js');
 
+var orm = {
+
+    select: function(col, table, cb)
+    {
+
+        var queryString = "SELECT ?? FROM ??";
+        connection.query(queryString, [col, table], function(err, result){
+                if(err)throw err;
+
+              console.log(result);
+
+              cb(err, result);
+
+        });
+
+      }
+
+
+
+
+
+}
 
 
 
